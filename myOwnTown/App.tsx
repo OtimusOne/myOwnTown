27e39c1126bbcf64b1a1eff1090014cbaf0eff2f
OtimusOne <otimusone@yahoo.com>
2019-11-01 19:04:18 +0200

    test

--- myOwnTown/App.tsx
+++ myOwnTown/App.tsx
@@ -1,12 +1,77 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Image } from 'react-native';
+import { Icon } from 'react-native-elements';
+import BottomNavigation, { FullTab } from 'react-native-material-bottom-navigation';
+export default class App extends React.Component {
+  tabs = [
+    {
+      key: 'map',
+      icon: 'map',
+      label: 'Harta',
+      barColor: '#388E3C',
+      pressColor: 'rgba(255, 255, 255, 0.16)',
+    },
+    {
+      key: 'news',
+      icon: 'star',
+      label: 'Anunturi',
+      barColor: '#B71C1C',
+      pressColor: 'rgba(255, 255, 255, 0.16)',
+    },
+    {
+      key: 'votes',
+      icon: 'comment',
+      label: 'Voturi',
+      barColor: '#E64A19',
+      pressColor: 'rgba(255, 255, 255, 0.16)',
+    },
+  ];
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+  renderIcon = icon => ({ isActive }) => <Icon size={24} color="white" name={icon} />;
+
+  renderTab = ({ tab, isActive }) => (
+    <FullTab
+      isActive={isActive}
+      key={tab.key}
+      label={tab.label}
+      renderIcon={this.renderIcon(tab.icon)}
+    />
   );
+  state = { activeTab: 'map' };
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          {this.state.activeTab == 'map' && (
+            <Image
+              resizeMode={'cover'}
+              style={{ width: '100%' }}
+              source={require('./assets/timisoara.png')}
+            />
+          )}
+          {this.state.activeTab == 'news' && (
+            <Image
+              resizeMode={'contain'}
+              style={{ width: '100%' }}
+              source={require('./assets/news.jpg')}
+            />
+          )}
+          {this.state.activeTab == 'votes' && (
+            <Image
+              resizeMode={'contain'}
+              style={{ width: '100%' }}
+              source={require('./assets/reddit.jpg')}
+            />
+          )}
+        </View>
+        <BottomNavigation
+          onTabPress={newTab => this.setState({ activeTab: newTab.key })}
+          renderTab={this.renderTab}
+          tabs={this.tabs}
+        />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
