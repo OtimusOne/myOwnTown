c7b691e4c21e452b9ac0522d32315248a170c198
DanMotataeanu <anca.pocovnicu91@e-uvt.ro>
2019-11-09 15:41:05 +0200

    html modal

--- myOwnTown/components/MapModal.tsx
+++ myOwnTown/components/MapModal.tsx
@@ -44,10 +44,9 @@ export default class MapModalScreen extends React.Component<MapModalProps, State
     componentDidMount() {
         firestore.collection("markers").doc(this.props.id).get().then(async (doc) => {
             if (doc.exists) {
-                const {title, text, description, img} = doc.data();
-                const ref = storage.ref(img);
-                const imgUrl = await ref.getDownloadURL();
-                this.setState({title, text, description, imgUrl});
+                const {title, text, description} = doc.data();
+                this.setState({title, text, description});
+
             }
         });
     }
@@ -63,22 +62,20 @@ export default class MapModalScreen extends React.Component<MapModalProps, State
                              borderColor: '#388E3C',
                              borderRadius: 20,
                          }}>
-                    <ScrollView>
-                        {this.state.readyToShow && this.state.text !== null ?
-                            <View>
-                                <Text style={[{alignSelf:"center"},material.title]}>{this.state.title}</Text>
-                                <Text style={[{alignSelf:"center"},material.subheading]}>{this.state.description}</Text>
-
-                                <HTML style={{alignSelf:"center"}} html = {this.state.text} imagesMaxWidth={Dimensions.get('window').width - 30}/>
-                            </View>
+                    <View>
+                        <Text style={[{alignSelf:"center"},material.title]}>{this.state.title}</Text>
+                        <Text style={[{alignSelf:"center"},material.subheading]}>{this.state.description}</Text>
+                        <ScrollView>
+                        {this.state.text !== null ?
+                            <HTML style={{alignSelf:"center", height:"80%"}} html = {this.state.text} imagesMaxWidth={Dimensions.get('window').width - 30}/>
                             : <Progress.Pie
                                 style={{alignSelf:"center"}}
                                 progress={80}
                                 indeterminate
                             />
                         }
-                        <Image style={{height: this.state.imgHeight, width: this.state.imgWidth, alignSelf:"center"}} source={{uri: this.state.imgUrl}} onLoadEnd={() => this.setState({readyToShow:true, imgHeight:0, imgWidth:0})}/>
-                    </ScrollView>
+                        </ScrollView>
+                    </View>
                 </Overlay>
         )
     }
