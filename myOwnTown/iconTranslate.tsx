dcc08b8887f09ef432d914c553004e7f6b6decec
OtimusOne <otimusone@yahoo.com>
2019-11-09 16:30:06 +0200

    new icons

--- myOwnTown/iconTranslate.tsx
+++ myOwnTown/iconTranslate.tsx
@@ -1,17 +1,14 @@
-import {Icon} from "react-native-elements";
-import React from "react";
+import { Icon } from 'react-native-elements';
+import React from 'react';
 
 // eslint-disable-next-line import/prefer-default-export
 export const iconName2Icon = {
-        "ball": <Icon
-            size={25}
-            name='ios-american-football'
-            type='ionicon'
-            color='#517fa4'
-        />,
-        "heart": <Icon
-            size={25}
-            name='heartbeat'
-            type='font-awesome'
-            color='#f50'/>
+  ball: <Icon size={25} name="md-football" type="ionicon" color="#517fa4" />,
+  info: <Icon size={25} name="info-circle" type="font-awesome" color="#f50" />,
+  heart: <Icon size={25} name="heartbeat" type="font-awesome" color="#f50" />,
+  star: <Icon size={25} name="star" type="font-awesome" color="#f50" />,
+  warning: <Icon size={25} name="warning" type="font-awesome" color="#f50" />,
+  music: <Icon size={25} name="music" type="font-awesome" color="#f50" />,
+  book: <Icon size={25} name="book" type="font-awesome" color="#f50" />,
+  computer: <Icon size={25} name="computer" type="material-cons" color="#f50" />,
 };
